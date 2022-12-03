cd5186014908751b9afec404861623bc69454ccf
Vladimir Leveryev <volodya.leveryev@gmail.com>
2022-12-04 00:33:32 +0900

    Сохранение данных пользователей во время разговора

--- data/persistance.py
+++ data/persistance.py
@@ -1,8 +1,9 @@
 import json
-from typing import Optional, Tuple
+from collections import defaultdict
+from typing import DefaultDict, Optional, Tuple
 
 from telegram.ext import BasePersistence
-from telegram.ext.utils.types import ConversationDict
+from telegram.ext.utils.types import UD, ConversationDict
 
 from data.base import exec_query
 
@@ -12,11 +13,47 @@ class YdbPersistence(BasePersistence):
 
     def __init__(self) -> None:
         super().__init__(
-            store_user_data=False,
+            store_user_data=True,
             store_chat_data=False,
             store_bot_data=False,
             store_callback_data=False,
         )
+
+    def get_user_data(self) -> DefaultDict[int, UD]:
+        """Прочитать данные диалогов с пользователями"""
+        query = """
+            SELECT user_id, data FROM user_data;
+        """
+        result = defaultdict(dict)
+        for row in exec_query(query):
+            result[row["user_id"]] = json.loads(row["data"])
+        return result
+
+    def update_user_data(self, user_id: int, data: UD) -> None:
+        """Обновить данные диалога с пользователем"""
+        if data:
+            # Обновить разговор
+            query = """
+                DECLARE $user_id AS Uint64;
+                DECLARE $data AS Json;
+                UPSERT INTO user_data (user_id, data, updated)
+                VALUES ($user_id, $data, CurrentUtcDatetime());
+            """
+            params = {
+                "$user_id": user_id,
+                "$data": json.dumps(data),
+            }
+            exec_query(query, params)
+        else:
+            # Удалить разговор
+            query = """
+                DECLARE $user_id AS Uint64;
+                DELETE FROM user_data WHERE user_id == $user_id
+            """
+            params = {
+                "$user_id": user_id,
+            }
+            exec_query(query, params)
 
     def get_conversations(self, name: str) -> ConversationDict:
         """Прочитать состояние разговора из базы данных"""
